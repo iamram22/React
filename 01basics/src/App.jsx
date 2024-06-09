524f759d7c80085a1dcd53a47979c4b10a14c46d
Ramakrishna Kumbar <ramlikescode@gmail.com>
2024-06-09 22:42:17 +0530

    Classes, components, and other basics

--- 01basics/src/App.jsx
+++ 01basics/src/App.jsx
@@ -1,10 +1,33 @@
+import "./App.css"
+//component names should always start with a capital letter (JSX)
+function FirstButton(){
+  return(
+    <button>Creating my first button in react</button>
+  );
+};
+
+//
+
+function AboutThisPage(){
+  return(
+    <>
+      <h1 className="about">About</h1>
+      <p>Hello this my First "P" tag in React <br/> I hope this runs..!!</p>
+    </>
+  );
+};
+
 
 
 function App() {
  
   return (
-    <h1>My first React App using Vite</h1>
-  );
+    <>
+      <h1>My first React App using Vite</h1>
+      <FirstButton/>
+      <AboutThisPage></AboutThisPage>
+    </> //only one component can be returned
+  ); 
 };
 
 export default App;
