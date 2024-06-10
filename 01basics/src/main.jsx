88b8107435e117967c86af1348b5a35a62375533
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-06-10 21:42:02 +0530

    component arranging

--- 01basics/src/main.jsx
+++ 01basics/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import UserInfo from "./AppTwo.jsx"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <UserInfo/>
   </React.StrictMode>,
 )
