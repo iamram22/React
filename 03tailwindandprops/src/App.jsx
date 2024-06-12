33849c72aa90df87970a35f7130789117bebc1ba
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-06-12 13:02:13 +0530

    Tailwind CSS integration and props integration, re-usable components

--- 03tailwindandprops/src/App.jsx
+++ 03tailwindandprops/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from "./components/card"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <>
-      <h1 className="bg-green-400 text-black p-4 rounded-xl">Tailwind CSS</h1>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-5">Tailwind CSS</h1>
+      <Card username = "props" btnText="using props"/>
+      <Card username = "This is how props work in React"/>
+      <Card/>
    </>
   )
 }
