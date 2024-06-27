8d4a4c5560eb9c09ecb47b1b6bc18290175aad7e
Ramakrishna Kumbar <ramlikestocode@gmail.com>
2024-06-27 22:12:22 +0530

    Currency Converter

---
+++ 06currencyConverter/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl bg-orange-500 rounded p-2">Curreny Converter APP</h1>
+    </>
+  )
+}
+
+export default App
